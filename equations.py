4a9eb3a3ffa6bb7f79afaca0b6bfc8fb06a70e09
tamar3162 <tamif3162@gmail.com>
2021-03-19 17:25:18 +0200

    first try

--- equations.py
+++ equations.py
@@ -17,43 +17,49 @@ def exponent (x):
         n += 1
     return res
 
-def len (x):
-    y_n=x-1.0
-    ans = 1
-    while ans>0.001:
-        y_n1=y_n+2*((x-exponent(y_n))/(x+exponent(y_n)))
-        ans = y_n-y_n1
-        if ans<0:
-            ans=-ans
-        y_n=y_n1
-    y_n1=float(y_n1)
-    if(y_n1<=0):
+def Ln (x):
+    if(x<=0):
         return 0
     else:
+        y_n=x-1.0
+        ans = 1
+        while ans>0.001:
+            y_n1=y_n+2*((x-exponent(y_n))/(x+exponent(y_n)))
+            ans = y_n-y_n1
+            if ans<0:
+                ans=-ans
+            y_n=y_n1
+            y_n1=float(y_n1)
         return y_n1
 
 def XtimesY(x,y):
     if(x<0 or y<0):
         return 0
     else:
-        if(x==0):
-            return 1;
-        else:
-            return (exponent(y*len(x)))
+        return(exponent(y*Ln(x)))
 
 def sqrt(x,y):
-    x=1/x
-    return (XtimesY(y,x))
+    if(y<0):
+        return 0
+    elif(y==0):
+        return 1
+    else:
+        x=1/x
+        return (XtimesY(y,x))
 
 def calculate(x):
-    temp=1
-    i=1
-    while i<=x:
-        temp =temp*7
-        i+=1
-    return((1/x)*exponent(x)*sqrt(x,x)*temp)
-   
-print(XtimesY(-2,2))   
+    if(x<0 or x==0):
+        res=0
+    else:
+        temp=1
+        i=1
+        while i<=x:
+            temp =temp*7
+            i+=1
+        res=((1/x)*exponent(x)*sqrt(x,x)*temp)
+        res=float(res)
+    return res
+     
     
     
     
